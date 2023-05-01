663e1b0070897ed235af9036037b0d50fc1a1bdd
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-05-01 11:36:44 -0500

    project: basics for routing

---
+++ src/server/controllers/index.ts
@@ -0,0 +1,5 @@
+import { Request, Response } from 'express'
+
+export function encryptController(_req: Request, _res: Response) {}
+
+export function decryptController(_req: Request, _res: Response) {}

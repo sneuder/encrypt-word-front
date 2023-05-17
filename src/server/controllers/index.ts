768ca2f9e7ec8b9cc40bca9e931960412de252f0
Esneider Ospina Oviedo <sneuder8@gmail.com>
2023-05-16 23:01:40 -0500

    feat: functions to order, split and multiply

--- src/server/controllers/index.ts
+++ src/server/controllers/index.ts
@@ -1,5 +1,9 @@
 import { Request, Response } from 'express'
+import encrypt from '../services/encrypt'
 
-export function encryptController(_req: Request, _res: Response) {}
+export function encryptController(req: Request, res: Response) {
+  const { words, keyword } = req.body
+  res.json(encrypt.process(words, keyword))
+}
 
 export function decryptController(_req: Request, _res: Response) {}
